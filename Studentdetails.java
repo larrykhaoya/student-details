4198842b7490654a8ac37451bf5fafcf99d80967
larrykhaoya <151062200+larrykhaoya@users.noreply.github.com>
2023-11-16 20:24:22 +0300

    Add files via upload

---
+++ Studentdetails.java
@@ -0,0 +1,17 @@
+public class Studentdetails {
+   public static void main(String[]args) {
+     String course= "DIP IT";
+     String name="LARRY KHAOYA";
+     String reg="22/06220";
+     String dob="9/10/2003";
+     String vision="EXCELLENCE";
+     String favlang= "JAVA";
+
+     System.out.println("Course:"+ course);
+     System.out.println("Name:" + name);
+     System.out.println("Registration Number :" + reg);
+     System.out.println("Date of Birth:"+ dob );
+     System.out.println("Vision:" + vision );
+     System.out.println("Favourite Programming Language:" + favlang);
+  }
+}
